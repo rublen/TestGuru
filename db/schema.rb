eb2ffb8e18ff54188084463933ccf05a33ec08a2
lena <rublen77@gmail.com>
2018-06-09 02:25:37 +0300

    models_task: added refrences, similar migrations were gathered in one

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180608221404) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20180608221404) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20180608221404) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level", default: 0
     t.integer "category_id"
     t.datetime "created_at", null: false
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20180608221404) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

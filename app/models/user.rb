a3f3529577d089b764d276618283977d0eb55303
lena <rublen77@gmail.com>
2018-06-12 07:29:22 +0300

    ActiveRecord_task: after refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
   def passed_tests_by_level(level)
-    list = []
-    TestPassage.where(user_id: id).pluck(:test_id).map do |i|
-      test = Test.find(i)
-      list << test.title if test.level == level
-    end
-    list
+    Test.where(id: TestPassage.where(user_id: id).pluck(:test_id), level: level)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,48 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-User.create(name: 'student0')
+user = User.create!(name: 'student0')
 
-Category.create(title: 'Ruby')
-Category.create(title: 'Ruby on Rails')
-Category.create(title: 'HTML')
-Category.create(title: 'JS')
+categories = Category.create! [{title: 'Ruby'}, {title: 'Ruby on Rails'}, {title: 'HTML'}, {title: 'JS'}]
 
-def create_test(title, level, category_title)
-  Test.create({
-    title: title,
-    level: level,
-    category_id: Category.find_by(title: category_title).id
-  })
-end
+tests = Test.create! [
+  { title: 'Ruby Basics', level: 0, category_id: categories[0].id },
+  { title: 'Ruby Metaprogramming', level: 2, category_id: categories[0].id },
+  { title: 'ActiveRecord', level: 1, category_id: categories[1].id },
+  { title: 'JS Basics', level: 0, category_id: categories[3].id }
+]
 
-create_test('Ruby Basics', 0, 'Ruby')
-create_test('Ruby Metaprogramming', 2, 'Ruby')
-create_test('ActiveRecord', 1, 'Ruby on Rails')
-create_test('JS Basics', 0, 'JS')
+questions = Question.create! [
+  { body: 'What does self mean in Ruby?', test_id: tests[1].id },
+  { body: 'What are scope gates in Ruby?', test_id: tests[1].id },
+  { body: 'What is ORM?', test_id: tests[2].id },
+  { body: 'How to get color for text in javascript?', test_id: tests[3].id }
+]
 
-def create_question(body, test_title)
-  Question.create({
-    body: body,
-    test_id: Test.find_by(title: test_title).id
-  })
-end
+Answer.create! [
+  { body: 'Instance method', question_id: questions[0].id, correct: false },
+  { body: 'Instance variable', question_id: questions[0].id, correct: false },
+  { body: 'Default object', question_id: questions[0].id, correct: true },
 
-create_question('What does self mean in Ruby?', 'Ruby Metaprogramming')
-create_question('What are scope gates in Ruby?', 'Ruby Metaprogramming')
-create_question('What is ORM?', 'ActiveRecord')
-create_question('How to get color for text in javascript?', 'JS Basics')
+  { body: 'Braces { }', question_id: questions[1].id, correct: false },
+  { body: 'Brackets [ ]', question_id: questions[1].id, correct: false },
+  { body: 'Key word "def"', question_id: questions[1].id, correct: true },
+  { body: 'Key word "class"', question_id: questions[1].id, correct: true },
+  { body: 'Key word "module"', question_id: questions[1].id, correct: true },
 
-def create_answer(body, key_word, correct = false)
-  question = Question.pluck(:body).find { |quest| quest.include? key_word }
-  Answer.create({
-    body: body,
-    question_id: Question.find_by(body: question).id
-  })
-end
+  { body: 'Object-relational mapping', question_id: questions[2].id, correct: true },
+  { body: 'Object\'s record management', question_id: questions[2].id, correct: false },
+  { body: 'Something else', question_id: questions[2].id, correct: false },
 
-create_answer('Instance method', 'self')
-create_answer('Instance variable', 'self')
-create_answer('Default object', 'self', true)
+  { body: 'document.getElementById("myP").style.color', question_id: questions[3].id, correct: true },
+  { body: '<p>.get_color', question_id: questions[3].id, correct: false },
+  { body: 'document.getElementById("myP").color', question_id: questions[3].id, correct: false },
+]
 
-create_answer('Braces { }', 'gates')
-create_answer('Brackets [ ]', 'gates')
-create_answer('Key word "def"', 'gates', true)
-create_answer('Key word "class"', 'gates', true)
-create_answer('Key word "module"', 'gates', true)
+TestPassage.create! [
+  { test_id: tests[0].id, user_id: user.id },
+  { test_id: tests[1].id, user_id: user.id },
+  { test_id: tests[2].id, user_id: user.id },
+  { test_id: tests[3].id, user_id: user.id },
+]
 
-create_answer('Object-relational mapping', 'ORM', true)
-create_answer('Object\'s record management', 'ORM')
-create_answer('Something else', 'ORM')
-
-create_answer('document.getElementById("myP").style.color', 'get color', true)
-create_answer('<p>.get_color', 'get color')
-create_answer('document.getElementById("myP").color', 'get color')
-
-def create_test_pass(test_title, user_name)
-  TestPassage.create({
-    test_id: Test.find_by(title: test_title).id,
-    user_id: User.find_by(name: user_name).id
-  })
-end
-
-create_test_pass('Ruby Metaprogramming', 'student0')
-create_test_pass('ActiveRecord', 'student0')
-create_test_pass('JS Basics', 'student0')

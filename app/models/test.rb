71cfe9f729f4e05c7713a136761060eaacf3be72
lena <rublen77@gmail.com>
2018-06-26 23:53:22 +0300

    associations_task: initial commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :test_passages
+  has_many :users, through: :test_passages
+  belongs_to :author
+
   def self.by_category(name)
     self.joins('JOIN categories ON category_id = categories.id')
       .where(categories: { title: name })

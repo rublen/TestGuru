209e172e3342f36f55dca93ccf926f0f41b5e40a
lena <rublen77@gmail.com>
2018-06-28 02:16:13 +0300

    have problems with author

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,10 +3,10 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :test_passages
   has_many :users, through: :test_passages
-  belongs_to :author
+  belongs_to :author, class_name: :'User', foreign_key: :'user_id'
 
   def self.by_category(name)
-    self.joins('JOIN categories ON category_id = categories.id')
+    joins('JOIN categories ON category_id = categories.id')
       .where(categories: { title: name })
       .order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,9 @@
 class User < ApplicationRecord
   has_many :test_passages
-  has_many :users, through: :test_passages
+  has_many :tests, through: :test_passages
 
   def passed_tests_by_level(level)
     Test.where(level: level, test_passages: { user_id: id })
       .joins("JOIN test_passages ON tests.id = test_id")
-  end
-end
-
-class Author < User
-  has_many :tests
-
-  def initialize(type = :aitor)
-    @type = type
   end
 end

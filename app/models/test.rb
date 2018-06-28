46d62df20def11215f2e340fef167a15e9fd86fa
lena <rublen77@gmail.com>
2018-06-28 20:39:35 +0300

    done

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :users, through: :test_passages
   has_many :questions
   has_many :test_passages
-  has_many :users, through: :test_passages
-  belongs_to :author, class_name: :'User', foreign_key: :'user_id'
 
   def self.by_category(name)
-    joins('JOIN categories ON category_id = categories.id')
-      .where(categories: { title: name })
-      .order(title: :desc).pluck(:title)
+    joins(:category).where(categories: { title: name })
+    .order(title: :desc).pluck(:title)
   end
 end

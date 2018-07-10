bd6376e98f86d3ae7d203353b37fd829ed91fbe0
lena <rublen77@gmail.com>
2018-07-10 18:46:35 +0300

    business_logic_task: test_passages work

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,9 +1,18 @@
 class TestPassagesController < ApplicationController
-  before_action :set_test_passage
+  before_action :set_test_passage, only: %i[show update result]
+
+  rescue_from NoMethodError, with: :rescue_with_test_without_questions
+
   def show
   end
 
   def update
+    @test_passage.accept!(params[:answer_ids])
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
   def result
@@ -12,5 +21,10 @@ class TestPassagesController < ApplicationController
   private
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def rescue_with_test_without_questions
+    @test = @test_passage.test
+    redirect_to test_path(@test), notice: 'Test has no questions yet.' if @test.questions.empty?
   end
 end

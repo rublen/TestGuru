94311ca81dc4fb4e42e947b9b6d2ebd97e0d7467
lena <rublen77@gmail.com>
2018-07-21 13:10:16 +0300

    devise_task: name for registration and flash[:notice]

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    flash[:notice] = "Hi, #{current_user.first_name}!"
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'tests#index'
 
   devise_for :users, path: :gurus,
-              path_names:  { sign_in: :login, sign_out: :logout }
+              path_names:  { sign_in: :login, sign_out: :logout },
+              controllers: { sessions: 'users/sessions', registrations: 'users/registrations'}
 
   resources :tests, only: :index do
     post :start, on: :member

9a5af632d952b37beff36efa44e38b952294f333
lena <rublen77@gmail.com>
2018-08-01 11:41:13 +0300

    bootstrap task: supposed to be done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
-  def after_sign_in_path_for(user)
-    stored_location_for(user) ||
-      if user.admin?
-        admin_tests_path
-      else
-        super
-      end
-  end
-
   def default_url_options
     I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale  }
   end

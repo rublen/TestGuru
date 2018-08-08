e86d2ed08387b6b694487d355c5bdfa98563b336
lena <rublen77@gmail.com>
2018-08-08 06:35:49 +0300

    js: scrit doesnt work

--- app/assets/javascripts/utilites/compare_passwords.js
+++ app/assets/javascripts/utilites/compare_passwords.js
@@ -1,22 +1,23 @@
 document.addEventListener('turbolinks:load', function() {
-  var form = document.querySelector('.new_user')
-  var password_confirmation = form.querySelector('#user_password_confirmation').value
-  
-  if(form && password_confirmation) { 
-    form.addEventListener('onsubmit', validate(form))
+  var control = document.querySelector('.pass-conf')
+  if (control) {
+    var password = control.querySelector('.pass1').value
+    var confirmation_elem = control.querySelector('.pass2')
+
+    if(confirmation_elem.value) {
+      confirmation_elem.addEventListener('input', validate())
+    }
   }
 })
 
-function validate(form) {
-  var elem1 = form.querySelector('#user_password')
-  var elem2 = form.querySelector('#user_password_confirmation')
-  
-  if(elem1.value === elem2.value) {
-    elem2.querySelector('.octicon-check').classList.remove('hide')
-    elem2.querySelector('.octicon-circle-slash').classList.add('hide')
+function validate() {
+  var confirmation = confirmation_elem.value
+  var len = confirmation.length
+  if(password[len-1] === confirmation[len-1]) {
+    this.querySelector('.octicon-check').classList.remove('hide')
+    this.querySelector('.octicon-circle-slash').classList.add('hide')
   } else {
-    elem2.querySelector('.octicon-circle-slash').classList.remove('hide')
-    elem2.querySelector('.octicon-check').classList.add('hide')
+    this.querySelector('.octicon-circle-slash').classList.remove('hide')
+    this.querySelector('.octicon-check').classList.add('hide')
   }
 }
- 

cbf16fdab933142069b85bc37bcc9062c501eb80
rublen <rublen77@gmail.com>
2018-08-08 11:31:45 +0000

    js: passwords comparison works

--- app/assets/javascripts/utilites/compare_passwords.js
+++ app/assets/javascripts/utilites/compare_passwords.js
@@ -1,22 +1,28 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('.pass-conf')
+  var control = document.querySelector('.new_user')
+  
   if (control) {
-    var password = control.querySelector('.pass1').value
-    var confirmation_elem = control.querySelector('.pass2')
-    confirmation_elem.addEventListener('input', validate(control, password))
-    // confirmation_elem.oninput = validate(control)
+    var confirmation_elem = control.querySelector('#user_password_confirmation')
+    confirmation_elem.addEventListener("change", validate)
   }
 })
 
-function validate(form, password) {
-  var confirmation = form.querySelector('.pass2').value
-  var len = confirmation.length
-  if(!len) { return }
-  if(password[len-1] === confirmation[len-1]) {
-    form.querySelector('.octicon-check').classList.remove('hide')
-    form.querySelector('.octicon-circle-slash').classList.add('hide')
+function validate() {
+  var password = document.querySelector('#user_password').value
+  var confirmation_elem = document.querySelector('#user_password_confirmation')
+  var confirmation = confirmation_elem.value
+  
+  if(!confirmation) { 
+    document.querySelector('.octicon-check').classList.add('hide')
+    document.querySelector('.octicon-circle-slash').classList.add('hide')
+    return
+  }
+  
+  if(password == confirmation) {
+    document.querySelector('.octicon-check').classList.remove('hide')
+    document.querySelector('.octicon-circle-slash').classList.add('hide')
   } else {
-    form.querySelector('.octicon-check').classList.add('hide')
-    form.querySelector('.octicon-circle-slash').classList.remove('hide')
+    document.querySelector('.octicon-check').classList.add('hide')
+    document.querySelector('.octicon-circle-slash').classList.remove('hide')
   }
 }

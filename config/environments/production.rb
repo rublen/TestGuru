20e43e58ad2a3583edc8ad176eafe7dddc3a5b25
lena <rublen77@gmail.com>
2018-08-13 08:38:57 +0300

    deploy: before db:seeds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     adress: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD']
+    password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }

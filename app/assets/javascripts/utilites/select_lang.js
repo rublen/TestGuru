ce8884a518582301887672a4b22143736bbe9467
lena <rublen77@gmail.com>
2018-08-14 15:25:14 +0300

    deploy: corrections7

--- app/assets/javascripts/utilites/select_lang.js
+++ app/assets/javascripts/utilites/select_lang.js
@@ -1,31 +1,31 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('.selectlang');
+  // var control = document.querySelector('.selectlang');
 
-  if(control) {
-    var url = window.location.href;
-    control.value = getLang();
-    control.addEventListener('change', switchLang);
-  };
+  // if(control) {
+  //   var url = window.location.href;
+  //   control.value = getLang();
+  //   control.addEventListener('change', switchLang);
+  // };
 
-  function getLang() {
-    if (url.includes('lang=')) {
-      var langParam = url.match(/lang=../)[0];
-      return langParam.substr(langParam.length-2);
-    } else {
-      return 'en';
-    }
-  }
+  // function getLang() {
+  //   if (url.includes('lang=')) {
+  //     var langParam = url.match(/lang=../)[0];
+  //     return langParam.substr(langParam.length-2);
+  //   } else {
+  //     return 'en';
+  //   }
+  // }
 
-  function switchLang() {
-    if (url.includes('?')) {
-      if (url.includes('lang=')) {
-        var langParam = url.match(/lang=../)[0];
-        window.location.href = url.replace(langParam, 'lang=' + control.value);
-      } else {
-        window.location.href = url + '&lang=' + control.value;
-      }
-    } else {
-      window.location.href = url + '?lang=' + control.value;
-    }
-  }
+  // function switchLang() {
+  //   if (url.includes('?')) {
+  //     if (url.includes('lang=')) {
+  //       var langParam = url.match(/lang=../)[0];
+  //       window.location.href = url.replace(langParam, 'lang=' + control.value);
+  //     } else {
+  //       window.location.href = url + '&lang=' + control.value;
+  //     }
+  //   } else {
+  //     window.location.href = url + '?lang=' + control.value;
+  //   }
+  // }
 });

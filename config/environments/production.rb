69de7c58b0ba4fa6a0b9bb8b7d95600fc0640ac2
lena <rublen77@gmail.com>
2018-08-16 09:43:42 +0300

    deploy: 25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     adress: 'smtp.gmail.com',
-    port: 587,
+    port: 25,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
